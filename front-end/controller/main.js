751d8878407e8969638d603418eb546d3f8bcaa5
ThuongDT <thuongdt@ezg.vn>
2025-05-26 13:17:21 +0700

    add angular js in project

---
+++ front-end/controller/main.js
@@ -0,0 +1,17 @@
+var app = angular.module("myApp", ['ngRoute'])
+
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/table', {
+            templateUrl: 'view/table.html',
+            controller: tableCtrl,
+        })
+        .otherwise({
+            redirectTo: '/table',
+        })
+}
+)
+
+app.controller('myCtrl', function ($scope) {
+
+})
