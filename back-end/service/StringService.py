9e58973886cdc0077526688c120508b2a39fa7e6
ThuongDT <thuongdt@ezg.vn>
2025-05-28 22:24:56 +0700

    add handle get id from url

--- back-end/service/StringService.py
+++ back-end/service/StringService.py
@@ -1,3 +1,6 @@
+from urllib.parse import urlparse, parse_qs
+
+
 def has_dash(str_check):
     return "-" in str_check
 
@@ -43,5 +46,24 @@ def handle_to_get_uid(str_old):
         if not has_dash(str) and not has_no_lowercase(str) and not has_no_uppercase(str):
 
             return str
+
+    return None
+
+def extract_drive_id(url):
+    parsed_url = urlparse(url)
+    query_params = parse_qs(parsed_url.query)
+
+    # Với link dạng: https://drive.google.com/open?id=...
+    if 'id' in query_params:
+        return query_params['id'][0]
+
+    # Với link dạng: https://drive.google.com/file/d/FILE_ID/view
+    path_parts = parsed_url.path.split('/')
+    if 'file' in path_parts and 'd' in path_parts:
+        try:
+            idx = path_parts.index('d')
+            return path_parts[idx + 1]
+        except IndexError:
+            pass
 
     return None
