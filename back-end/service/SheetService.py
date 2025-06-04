c502994148002fe4b1d98e5b67d57378f0ae18ad
ThuongDT <thuongdt@ezg.vn>
2025-06-04 23:14:36 +0700

    add handle data from sheet

--- back-end/service/SheetService.py
+++ back-end/service/SheetService.py
@@ -2,6 +2,11 @@ import gspread
 
 from oauth2client.service_account import ServiceAccountCredentials
 
+from controller import APIController as api
+from service import StringService as strService
+
+from entity.User import User
+
 
 LINK_HEAD = "E:/project/security/"
 
@@ -13,6 +18,45 @@ def get_sheet(id_sheet, name_tab_sheet):
 
     return spreadsheet.worksheet(name_tab_sheet)
 
+def handl_data_fom_sheet(list_data_from_row):
+    # col 1 - case
+    # col 11, 13, 15 - user's content
+    # col 12, 14, 16 - link
+    # col 17 - device id and uid
+    # col 18 - user's mail
+    # col 19 - answer's QA
+    # col 21 - status of case from CS
+    # col 26 - status of case from QA
+
+    data_dict = {
+        "case": [0],
+        "get_id": [16],
+        "content": [10, 12, 14],
+        "link": [11, 13, 15],
+        "mail": [17],
+        "answer": [19],
+        "status": [21]
+    }
+
+    case = list_data_from_row[data_dict["case"][0]]
+
+    get_id = list_data_from_row[data_dict["get_id"][0]]
+    uid = strService.handle_to_get_uid(get_id)
+    device_id = strService.handle_to_get_device_id(get_id)
+
+    content = "\n".join(str(list_data_from_row[i]) for i in data_dict["content"] if list_data_from_row[i])
+    
+    link_before_clean = "\n".join(str(list_data_from_row[i]).strip() for i in data_dict["link"] if list_data_from_row[i])
+    link = strService.clean_link(link_before_clean)
+
+    id_bill =  api.api_get_id_bill(link)
+
+    mail = list_data_from_row[data_dict["mail"][0]]
+    answer = list_data_from_row[data_dict["answer"][0]]
+    status = list_data_from_row[data_dict ["status"][0]]
+
+    return User(case, uid, device_id, mail, content, link, id_bill, answer, status)
+
 def handle_get_data(sheet, cols_to_get, status = None):
     all_values = sheet.get_all_values()
     data_rows = all_values[1:]
@@ -22,21 +66,16 @@ def handle_get_data(sheet, cols_to_get, status = None):
         row_number = idx + 2  # vì idx bắt đầu từ 0, dòng thực là từ 2
         row_data = {"row": row_number}
 
-        empty = True
+        if status is not None and row[21] not in status:
+            continue
 
         for col_idx in cols_to_get:
-            if status is not None and status != row[20]:
-                continue
 
             value = row[col_idx] if col_idx < len(row) else ""
 
-            if value.strip() == "":
-                empty = False
-
             row_data[col_idx] = value.strip()
 
-        if not empty:
-            result.append(row_data)
+        result.append(row_data)
 
     return result
 
